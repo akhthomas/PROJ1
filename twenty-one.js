b12cf0625088b3c4368ae9cf48ca243cc16330ad
akhthomas <ms.kellanthomas@gmail.com>
2022-04-13 15:24:45 -0700

    some constants readded

--- twenty-one.js
+++ twenty-one.js
@@ -1,13 +1,13 @@
 
 
-// _____ MUSIC 
+// **** MUSIC ****
 function pause() {
     let pause = document.getElementById("audio");
     console.log(audio);
     return audio.paused ? audio.play() : audio.pause();
 };
 
-// ____ SCREEN FUNCTION
+// **** SCREEN FUNCTION ****
 function showHide() {
     const screenEl = document.getElementById('screen-1');
     const screen2El = document.getElementById('screen-2');
@@ -15,5 +15,13 @@ function showHide() {
     screenEl.style.display = 'none';
     screen2El.style.display = 'block';
 }
+
+// **** CONSTANTS ****
+const value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const suits = ["spades", "diamonds", "clubs", "hearts"];
+
+const winCondition = 21;
+
+
 
 
